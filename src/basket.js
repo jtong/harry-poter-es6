ccf8482f2102c0a12f7ed792f9fb7acc9e776ebe
jtong <tj19832@gmail.com>
2015-09-10 23:27:38 +0800

    jtong - refactor reduce length of function, lines less than 15

--- src/basket.js
+++ src/basket.js
@@ -10,33 +10,9 @@ export default class Basket {
             return this.basketItems[0].price;
         }
 
-        var discounts = [0,0,0,0,0,0];
-        do{
-            var discountLevel = this.basketItems.reduce((result, item)=>{
-                if(item.count > 0){
-                    item.count--;
-                    return result + 1;
-                }
-                return result;
-            }, 0);
-            discounts[discountLevel]++;
-        }while(discountLevel >1)
-        discounts.shift();
-
-        let sum = discounts.reduce((result, item, index) => {
-            if(index > 0){
-                return result + 8*item*(index+1)*(1-Discounts[index + 1]);
-            }
-            return result + 8*item
-        }, 0);
-
-        let leftItem = this.basketItems.find((item) => {
-            return item.count > 0;
-        });
-
-        if(leftItem){
-            sum += 8*leftItem.count;
-        }
+        var basketItems = this.basketItems;
+        var discounts = calculateMaxDiscounts(basketItems);
+        var sum = sumPriceWithDiscount(discounts, basketItems);
         return sum;
     }
 }
@@ -46,4 +22,38 @@ var Discounts = {
     3: 0.1,
     4: 0.2,
     5: 0.25
+};
+
+function sumPriceWithDiscount(discounts, basketItems) {
+    let sum = discounts.reduce((result, item, index) => {
+        if (index > 0) {
+            return result + 8 * item * (index + 1) * (1 - Discounts[index + 1]);
+        }
+        return result + 8 * item
+    }, 0);
+
+    let leftItem = basketItems.find((item) => {
+        return item.count > 0;
+    });
+
+    if (leftItem) {
+        sum += 8 * leftItem.count;
+    }
+    return sum;
+}
+
+function calculateMaxDiscounts(basketItems) {
+    var discounts = [0, 0, 0, 0, 0, 0];
+    do {
+        var discountLevel = basketItems.reduce((result, item)=> {
+            if (item.count > 0) {
+                item.count--;
+                return result + 1;
+            }
+            return result;
+        }, 0);
+        discounts[discountLevel]++;
+    } while (discountLevel > 1)
+    discounts.shift();
+    return discounts;
 }
